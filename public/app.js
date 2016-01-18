d2ed1b11c914ef4618672fa04e101e47493e852e
jorgeRafaelSJ <jorgerafaelsj@gmail.com>
2016-01-18 12:22:08 -0800

    Revert "Revert "initial commit, built table""
    This reverts commit 11310e13e1f8910239aa127c32dfba5c7a87f91b.

---
+++ public/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('cuatro', ['ui.router'])
+
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $stateProvider
+      	.state('game', {
+        	url: "/",
+        	templateUrl: 'templates/game',
+        	controller: 'GameController'
+      	});
+  
+
+    $urlRouterProvider.otherwise("/");
+
+	$locationProvider.html5Mode({
+    	enabled: true,
+    	requireBase: false
+	});
+}]);
